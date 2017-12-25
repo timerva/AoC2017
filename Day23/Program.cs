8cfb3fa0c3702a4207b1c29193e9f20cd33c41ae
Tim Ervasti <tim.ervasti@sogeti.se>
2017-12-25 17:01:37 +0100

    Dag 23, part 1 + Dag 24 - klar

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day23
 {
@@ -6,7 +9,106 @@ namespace Day23
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var instructions = File.ReadLines(@".\input.txt").Select(p => p.Split(' ')).Select(p => new Instruction { Type = (InstructionType)Enum.Parse(typeof(InstructionType), p[0]), Register = p[1], Action = p.Length == 3 ? p[2] : string.Empty, Executed = false }).ToArray();
+
+            var pgm = new Pgm(instructions);
+            while (true)
+            {
+                ProcessInstruction(pgm);
+                if (pgm.InstructionPos >= instructions.Length)
+                    break;
+            }
+            Console.WriteLine($"Day 23 - Assignment 1: The number of mul is: {pgm.NumberOfMul}");
+          
+          //  Console.WriteLine($"Day 23 - Assignment 2: The value of the H register is: {Part2()}");
         }
+
+    
+
+        static void ProcessInstruction(Pgm pgm)
+        {
+            var instruction = pgm.Instructions[pgm.InstructionPos];
+            var registers = pgm.Registers;
+            instruction.Executed = false;
+            if (!instruction.Register.IsNumeric() && !registers.ContainsKey(instruction.Register))
+            {
+                registers.Add(instruction.Register, 0);
+            }
+            long? actionValue = 0;
+            if (instruction.Action.IsNumeric())
+                actionValue = int.Parse(instruction.Action);
+            else
+            {
+                if (string.IsNullOrEmpty(instruction.Action))
+                { actionValue = null; }
+                else
+                {
+                    actionValue = registers[instruction.Action];
+                }
+            }
+
+            switch (instruction.Type)
+            {
+                case InstructionType.sub:
+                    registers[instruction.Register] -= actionValue.Value;
+                    break;
+                case InstructionType.jnz:
+                    if ((instruction.Register.IsNumeric() && int.Parse(instruction.Register) != 0) || registers[instruction.Register] != 0)
+                    {
+                        pgm.InstructionPos += actionValue.Value;
+                        return;
+                    }
+                    break;
+                case InstructionType.mul:
+                    pgm.NumberOfMul++;
+                    registers[instruction.Register] *= actionValue.Value;
+
+                    break;
+                case InstructionType.set:
+                    registers[instruction.Register] = actionValue.Value;
+                    break;
+            }
+            pgm.InstructionPos++;
+        }
+    }
+
+    public static class StringExt
+    {
+        public static bool IsNumeric(this string text)
+        {
+            return double.TryParse(text, out double test);
+        }
+    }
+
+    public class Pgm
+    {
+        public Dictionary<string, long> Registers;
+        public Instruction[] Instructions;
+
+        public long InstructionPos = 0;
+        public long NumberOfMul = 0;
+        public Pgm(Instruction[] instructions)
+        {
+            Registers = new Dictionary<string, long>();
+            Instructions = instructions;
+        }
+    }
+
+
+    public class Instruction
+    {
+        public InstructionType Type { get; set; }
+        public string Register { get; set; }
+        public string Action { get; set; }
+        public bool Executed { get; set; }
+    }
+
+
+    public enum InstructionType
+    {
+        set,
+        sub,
+        mul,
+        jnz
     }
 }
--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,12 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
 
 namespace Day24
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines(@".\input.txt");
+            var components = lines.Select(s => s.Split('/')).Select(s => new Component(int.Parse(s[0]), int.Parse(s[1]))).ToImmutableList();
+            Console.WriteLine($"Day 24 - Assignment 1: The strongest bridge has strength: {BuildBridgesPart1(components)}");
+            var resultPart2 = BuildBridgesPart2(components);
+            Console.WriteLine($"Day 24 - Assignment 2: The longest bridges is: {resultPart2.Item2} with strength: {resultPart2.Item1}");
+        }
+
+        static int BuildBridgesPart1(ImmutableList<Component> components, int connectValue = 0, int strength = 0)
+        {
+            var possibleNextComponents = components.Where(c => c.End1 == connectValue || c.End2 == connectValue).ToImmutableList();
+            if (possibleNextComponents.Count() == 0)
+                return strength;
+            List<int> bridges = new List<int>();
+
+            foreach (var nextCompontent in possibleNextComponents)
+            {
+                int bridgeStrength = strength + nextCompontent.End1 + nextCompontent.End2;
+                int newConnectValue = 0;
+                if (nextCompontent.End1 == connectValue)
+                    newConnectValue = nextCompontent.End2;
+                else
+                    newConnectValue = nextCompontent.End1;
+
+                var componentsLeft = components.Remove(nextCompontent);
+                bridges.Add(BuildBridgesPart1(componentsLeft, newConnectValue, bridgeStrength));
+            }
+            return bridges.Max();   
+        }
+
+        static Tuple<int,int> BuildBridgesPart2(ImmutableList<Component> components, int connectValue = 0, int strength = 0, int length = 0)
+        {
+            var possibleNextComponents = components.Where(c => c.End1 == connectValue || c.End2 == connectValue).ToImmutableList();
+            if (possibleNextComponents.Count() == 0)
+                return new Tuple<int, int>(strength, length);
+            List<Tuple<int,int>> bridges = new List<Tuple<int,int>>();
+
+            foreach (var nextCompontent in possibleNextComponents)
+            {
+                int bridgeStrength = strength + nextCompontent.End1 + nextCompontent.End2;
+                int bridgeLenght = length + 1;
+                int newConnectValue = 0;
+                if (nextCompontent.End1 == connectValue)
+                    newConnectValue = nextCompontent.End2;
+                else
+                    newConnectValue = nextCompontent.End1;
+
+                var componentsLeft = components.Remove(nextCompontent);
+                bridges.Add(BuildBridgesPart2(componentsLeft, newConnectValue, bridgeStrength, bridgeLenght));
+            }
+            return bridges.OrderByDescending(b => b.Item2).ThenByDescending(b => b.Item1).First();
+
+        }
+    }
+
+    public class Component
+    {
+        public int End1 { get; set; }
+        public int End2 { get; set; }
+        public bool Connected { get; set; }
+        public Component(int end1, int end2)
+        {
+            End1 = end1;
+            End2 = end2;
+            Connected = false;
         }
     }
 }
